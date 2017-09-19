e983cfc946db43558b3b52476cd7d1df6a286f53
lingchen <ling.146@osu.edu>
2017-09-18 22:04:39 -0400

    I added an ISprite interface and a class for super mario's crouching left

---
+++ FlugelMario/FlugelMario/Interfaces/ISprite.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlugelMario.Interfaces
+{
+    public interface ISprite
+    {
+        Texture2D Texture { get; set; }
+
+        void Update();
+
+        void Draw(SpriteBatch spriteBatch, Vector2 location);
+    }
+}
---
+++ FlugelMario/FlugelMario/Sprites/CrouchLeftBigMarioSprite.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint1Game.SpriteFactories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlugelMario.Sprites
+{
+    class CrouchLeftBigMarioSprite : ISprite
+    {
+        private int TextureX = (int)MarioSpriteFactory.Instance.CrouchLeftBigMarioCord.X;
+        private int TextureY = (int)MarioSpriteFactory.Instance.CrouchLeftBigMarioCord.Y;
+
+        public CrouchLeftBigMarioSprite(Texture2D texture) : base(texture)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 marioLocation)
+        {
+            int x = TextureX * MarioWidth;
+            int y = TextureY * MarioHeight;
+            int width = SourceRectangle.Width;
+            int height = SourceRectangle.Height;
+            SourceRectangle = new Rectangle(x, y, width, height);
+            base.Draw(spriteBatch, marioLocation);
+        }
+    }
+}
