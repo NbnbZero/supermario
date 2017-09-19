ca77c5e4b4c17083b7d32df75557fb10fac412f7
lingchen <ling.146@osu.edu>
2017-09-19 15:24:28 -0400

    Added 2 interfaces for future uses

---
+++ FlugelMario/Interfaces/IMario.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlugelMario.Interfaces
+{
+    interface IMario
+    {
+        IMarioState State { get; set; }
+        Vector2 Location { get; set; }
+        void TakeDamage();
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+        void CorrectLocation(bool bigState, bool crouchingState);
+    }
+}
---
+++ FlugelMario/Interfaces/IMarioState.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace FlugelMario.Interfaces
+{
+    interface IMarioState
+    {
+        ISprite StateSprite { get; set; }
+        void ChangeToRight();
+        void JumpOrStand();
+        void ChangeSizeToBig();
+        void ChangeSizeToSmall();
+        void Terminated();
+        void ChangeToLeft();
+        void Crouch();
+        void ChangeFireMode();
+    }
+}
