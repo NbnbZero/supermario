d2460ab0a35d209f0e762964b5732a2b23552ae4
lingchen <ling.146@osu.edu>
2017-09-18 22:33:27 -0400

    Some revises.

---
+++ FlugelMario/Sprites/CrouchLeftBigMarioSprite.cs
@@ -0,0 +1,30 @@
+﻿using FlugelMario.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlugelMario.Sprites
+{
+    class CrouchLeftBigMarioSprite : ISprite
+    {
+        public Texture2D Texture { get; set; }
+        Rectangle sourceRectangle;
+        private const int MarioWidth = 15;
+        private const int MarioHeight = 21;
+        private const int TextureX = 154;
+        private const int TextureY = 54;
+
+        public CrouchLeftBigMarioSprite()
+        {
+            sourceRectangle = new Rectangle(TextureX, TextureY, MarioWidth, MarioHeight);
+        }
+        public void Update() { }
+        public void Draw(SpriteBatch spriteBatch, Vector2 marioLocation)
+        {
+            Rectangle destinationRectangle = new Rectangle((int)marioLocation.X, (int)marioLocation.Y, MarioWidth, MarioHeight);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
