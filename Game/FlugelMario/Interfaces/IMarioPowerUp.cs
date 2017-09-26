44fadb2f418a53d630d6673d20558b4700f34576
lingchen <ling.146@osu.edu>
2017-09-26 00:44:01 -0400

    revises

---
+++ Game/FlugelMario/Interfaces/IMarioPowerUp.cs
@@ -0,0 +1,13 @@
+﻿using FlugelMario.Enums;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlugelMario.Interfaces
+{
+    public interface IMarioPowerUp
+    {
+        Shape MarioShape { get; set; }
+        Controller KeyboadrController { get; set; }
+        void Update();
+    }
+}
