fca1450033d3765657c5d2f0bdef3714f581ed50
Haobin Xu <xu.2079@osu.edu>
2017-10-30 21:28:10 -0400

    Fix Index and enemy/item movement.

--- Game/FlugelMario/Sprites/Items/SuperMushroomSprite.cs
+++ Game/FlugelMario/Sprites/Items/SuperMushroomSprite.cs
@@ -8,6 +8,8 @@ namespace SuperMario.Sprites.Items
 {
     class SuperMushroomSprite : ItemSprite
     {
+        private bool goLeft;
+        private bool goRight;
         public SuperMushroomSprite(Texture2D texture, Vector2 location, bool hidden) : base(texture, location, hidden)
         {
             Width = ItemSpriteFactory.Instance.SuperMushroomWith;
@@ -17,11 +19,30 @@ namespace SuperMario.Sprites.Items
             TextureY = (int)ItemSpriteFactory.Instance.SuperMushroomAnimation1.Y;
 
             TotalFrames = ItemSpriteFactory.Instance.SuperMushroomAnimeTotalFrame;
+
+            goLeft = false;
+            goRight = false;
+
         }
 
         public override void Update(Viewport viewport, Vector2 marioLocation)
         {
             Animate();
+            goLeft = Location.X < (marioLocation.X + viewport.Width / 2) && Location.X > marioLocation.X;
+            goRight = Location.X < marioLocation.X;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            if (goLeft)
+            {
+                Location = new Vector2(Location.X - 1, Location.Y);
+            }
+            else if (goRight)
+            {
+                Location = new Vector2(Location.X + 1, Location.Y);
+            }
+            base.Draw(spriteBatch, location);
         }
     }
 }
--- Game/FlugelMario/Sprites/Items/UpMushroomSprite.cs
+++ Game/FlugelMario/Sprites/Items/UpMushroomSprite.cs
@@ -8,6 +8,9 @@ namespace SuperMario.Sprites.Items
 {
     class UpMushroomSprite : ItemSprite
     {
+        private bool goLeft;
+        private bool goRight;
+
         public UpMushroomSprite(Texture2D texture, Vector2 location, bool hidden) : base(texture, location, hidden)
         {
             Width = ItemSpriteFactory.Instance.UpMushroomWith;
@@ -17,11 +20,29 @@ namespace SuperMario.Sprites.Items
             TextureY = (int)ItemSpriteFactory.Instance.UpMushroomAnimation1.Y;
 
             TotalFrames = ItemSpriteFactory.Instance.UpMushroomAnimeTotalFrame;
+            goLeft = false;
+            goRight = false;
         }
 
         public override void Update(Viewport viewport, Vector2 marioLocation)
         {
             Animate();
+            goLeft = Location.X < (marioLocation.X + viewport.Width / 2) && Location.X > marioLocation.X;
+            goRight = Location.X < marioLocation.X;
         }
+        public override void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            if (goLeft)
+            {
+                Location = new Vector2(Location.X - 1, Location.Y);
+            }
+            else if (goRight)
+            {
+                Location = new Vector2(Location.X + 1, Location.Y);
+            }
+            base.Draw(spriteBatch, location);
+        }
+
+
     }
 }
