baf2262d3af7677d7efe0b91c01fba7b9f5d43dd
Haobin Xu <xu.2079@osu.edu>
2017-11-04 16:15:07 -0400

    Sprite,Item and enemy interface inprovement.

--- Game/FlugelMario/Interfaces/IEnemy.cs
+++ Game/FlugelMario/Interfaces/IEnemy.cs
@@ -6,7 +6,11 @@ namespace SuperMario.Interfaces
     public interface IEnemy : IGameObject
     {
         IEnemyState State { get; set; }
+        bool CanUpdate();
         bool Alive { get; set; }
-        void Terminate();
+        bool Moving { get; set; }
+        Vector2 Velocity { get; set; }
+        void Terminate(String direction);
+        void ChangeDirection();
     }
 }
--- Game/FlugelMario/Interfaces/IItem.cs
+++ Game/FlugelMario/Interfaces/IItem.cs
@@ -1,8 +1,13 @@
-﻿namespace SuperMario.Interfaces
+﻿using Microsoft.Xna.Framework;
+
+namespace SuperMario.Interfaces
 {
     public interface IItem : IGameObject
     {
+        Vector2 Velocity { get; set; }
         bool IsCollected { get; set; }
         void Collect();
+        bool IsPreparing { get; set; }
+        
     }
 }
