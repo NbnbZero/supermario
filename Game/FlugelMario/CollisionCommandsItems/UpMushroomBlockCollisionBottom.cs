3766957cba6f32585cea2fba12b878e48885ee36
Haobin Xu <xu.2079@osu.edu>
2017-11-06 17:29:26 -0500

    Item Collision Command

---
+++ Game/FlugelMario/CollisionCommandsItems/UpMushroomBlockCollisionBottom.cs
@@ -0,0 +1,26 @@
+﻿using SuperMario.GameObjects;
+using SuperMario.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMario.CollisionCommandsItems
+{
+    class UpMushroomBlockCollisionBottom:ICollisionCommand
+    {
+        public UpMushroomBlockCollisionBottom()
+        {
+
+        }
+        public void Execute(IGameObject gameObject1, IGameObject gameObject2)
+        {
+            UpMushroom upMushroom = (UpMushroom)gameObject1;
+            if (gameObject2.GetType() == typeof(HiddenBlock) && upMushroom.Velocity.Y >= 0)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Game/FlugelMario/CollisionCommandsItems/UpMushroomBlockCollisionRight.cs
@@ -0,0 +1,34 @@
+﻿using SuperMario.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMario.CollisionCommandsItems
+{
+    class UpMushroomBlockCollisionRight : ICollisionCommand
+    {
+        public UpMushroomBlockCollisionRight()
+        {
+
+        }
+        public void Execute(IGameObject gameObject1, IGameObject gameObject2)
+        {
+            UpMushroom upMushroom = (UpMushroom)gameObject1;
+            if (upMushroom.IsPreparing)
+            {
+                return;
+            }
+            int upMushroomPreY = (int)(upMushroom.Destination.Y - (upMushroom.Velocity.Y - 1));
+            if (upMushroomPreY + upMushroom.Destination.Height <= gameObject2.Destination.Y)
+            {
+                return;
+            }else if (upMushroomPreY > gameObject2.Destination.Y + gameObject2.Destination.Height)
+            {
+                return;
+            }
+            upMushroom.ChangeDirection();
+        }
+    }
+}
