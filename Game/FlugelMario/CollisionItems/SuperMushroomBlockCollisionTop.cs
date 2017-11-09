a09a6a8fba8af22c4ea1324510a5c3799cfbf55c
Haobin Xu <xu.2079@osu.edu>
2017-11-09 14:12:18 -0500

    SuperMushroom block collision

--- Game/FlugelMario/CollisionItems/SuperMushroomBlockCollisionTop.cs
+++ Game/FlugelMario/CollisionItems/SuperMushroomBlockCollisionTop.cs
@@ -9,14 +9,16 @@ using System.Threading.Tasks;
 
 namespace SuperMario
 {
-    class SuperMushroomBlockCollisionTop:ICollisionCommand
+    class SuperMushroomBlockCollisionTop:ICommand
     {
-        public SuperMushroomBlockCollisionTop()
+        CollisionHandlerSuperMushroom myhandler;
+        public SuperMushroomBlockCollisionTop(CollisionHandlerSuperMushroom handler)
         {
-
+            myhandler = handler;
         }
-        public void Execute(IGameObject gameObject1, IGameObject gameObject2)
+        public void Execute()
         {
+            /*
             if (gameObject2.GetType() == typeof(HiddenBlock))
             {
                 HiddenBlock hiddenBlock = (HiddenBlock)gameObject2;
@@ -31,6 +33,22 @@ namespace SuperMario
             superMushroom.Location = new Vector2(superMushroom.Location.X, gameObject2.Location.Y - superMushroom.Destination.Height);
 
             superMushroom.Velocity = new Vector2(superMushroom.Velocity.X, 0);
+            */
+            if (myhandler.block.GetType() == typeof(HiddenBlock))
+            {
+                HiddenBlock hiddenBlock = (HiddenBlock)myhandler.block;
+                if (!hiddenBlock.Used) return;
+            }
+
+            SuperMushroom superMushroom = (SuperMushroom)myhandler.superMushroom1;
+            if (superMushroom.IsPreparing)
+            {
+                return;
+            }
+            myhandler.superMushroom1.Location = new Vector2(myhandler.superMushroom1.Location.X, myhandler.block.Location.Y - myhandler.superMushroom1.Destination.Height);
+
+            myhandler.superMushroom1.Velocity = new Vector2(myhandler.superMushroom1.Velocity.X, 0);
+
         }
     }
 }
