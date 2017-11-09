c823eb947ca24cf31c767841bba0f657d9350d74
yang.3260 <yang.3260@osu.edu>
2017-11-09 01:20:19 -0500

    FFFFFFIX

--- Game/FlugelMario/CollisionMario/MarioBlockCollisionCommand/MarioBlockBottom.cs
+++ Game/FlugelMario/CollisionMario/MarioBlockCollisionCommand/MarioBlockBottom.cs
@@ -12,10 +12,25 @@ namespace SuperMario.Commands
         {
             myhandler = handler;
         }
-
+      
         public void Execute()
         {
+            if (myhandler.mario.State.MarioShape == Shape.Dead)
+            {
+                return;
+            }
+            if (myhandler.block.GetType() == typeof(HiddenBlock) && myhandler.mario.Velocity.Y >= 0)
+            {
+                return;
+            }
+            myhandler.mario.Location = new Vector2(myhandler.mario.Destination.X, myhandler.block.Destination.Y + myhandler.block.Destination.Height - 1);
             myhandler.mario.Velocity = new Vector2(myhandler.mario.Velocity.X, -myhandler.mario.Velocity.Y);
+            if (!((myhandler.mario.State.MarioShape == Shape.Small ||
+                    myhandler.mario.State.MarioShape == Shape.StarSmall) &&
+                    (myhandler.block.GetType() == typeof(BrickBlock))))
+            {
+                myhandler.block.Trigger();
+            }
 
             if ((myhandler.mario.State.MarioShape == Shape.Small ||
                     myhandler.mario.State.MarioShape == Shape.StarSmall) &&
