ced6982f3969fc81572f90de22a615d3d6162af9
yang.3260 <yang.3260@osu.edu>
2017-11-08 22:15:51 -0500

    CollisionFrame

---
+++ Game/FlugelMario/CollisionMario/MarioBlockCollisionCommand/BlockMarioBottomCommand.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using SuperMario.Interfaces;
+
+namespace SuperMario.Commands
+{
+    class BlockMarioBottomCommand : ICommand
+    {
+        CollisionHandler myhandler;
+        public BlockMarioBottomCommand(CollisionHandler handler)
+        {
+            myhandler = handler;
+        }
+
+        public void Execute()
+        {
+            myhandler.mario.Velocity = new Vector2(myhandler.mario.Velocity.X, -myhandler.mario.Velocity.Y);
+        }
+    }
+}
---
+++ Game/FlugelMario/CollisionMario/MarioBlockCollisionCommand/BlockMarioTopCollisionCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using SuperMario.Interfaces;
+using SuperMario.Enums;
+namespace SuperMario.Commands
+{
+    class BlockMarioTopCollisionCommand:ICommand
+    {
+        CollisionHandler myhandler;
+        public BlockMarioTopCollisionCommand(CollisionHandler handler)
+        {
+            myhandler = handler;
+        }
+
+        public void Execute()
+        {
+            myhandler.mario.IsInAir = false;
+            myhandler.mario.Velocity = new Vector2(myhandler.mario.Velocity.X, 0);
+            myhandler.mario.Location = new Vector2(myhandler.mario.Destination.X, myhandler.block.Destination.Y - myhandler.mario.Destination.Height);
+            if (myhandler.mario.State.MarioShape == Shape.Dead)
+            {
+                return;
+            }
+        }
+    }
+}
