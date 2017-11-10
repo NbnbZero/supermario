344a04bb11f483a4f58eeb3a81b1ad06f2e57e9a
lingchen <ling.146@osu.edu>
2017-11-09 20:59:59 -0500

    1

--- Game/FlugelMario/CollisionEnemies/EnemyEnemyTwoSide.cs
+++ Game/FlugelMario/CollisionEnemies/EnemyEnemyTwoSide.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using SuperMario.Interfaces;
+using SuperMario.States.EnemyStates;
 
 namespace SuperMario.Commands
 {
@@ -13,6 +14,32 @@ namespace SuperMario.Commands
 
         public void Execute()
         {
+            handler.enemy.Location = new Vector2(handler.enemy.Location.X - 1, handler.enemy.Location.Y);
+            if (handler.enemy.GetType() == typeof(Goomba))
+            {
+                if(!handler.enemy.Alive) return;
+                if (handler.another.State.GetType() == typeof(KoopaDeadState) && handler.another.Velocity.X != 0)
+                {
+                    handler.enemy.Terminate("Left");
+                    return;
+                }
+            }
+            if (handler.enemy.GetType() == typeof(Koopa))
+            {
+                if (handler.enemy.State.GetType() == typeof(KoopaDeadState))
+                {
+                    return;
+                }
+                if (!handler.another.Alive)
+                {
+                    return;
+                }
+                if (handler.enemy.State.GetType() == typeof(KoopaDeadState) && handler.enemy.Velocity.X != 0)
+                {
+                    handler.another.Terminate("Left");
+                    return;
+                }
+            }
             handler.enemy.Location = new Vector2(handler.enemy.Location.X - 1, handler.enemy.Location.Y);
             handler.enemy.ChangeDirection();
             handler.another.ChangeDirection();
